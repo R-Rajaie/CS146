4d9069eb7f077b2e15e94c4ce4bd5a5dfecf87f6
Ryan Rajaie <ryan.rajaie04@gmail.com>
2024-01-31 14:27:47 -0800

    added basic files to give structure

---
+++ src/com/sjsu/cs146/homework/hw1/hw1.java
@@ -0,0 +1,7 @@
+package com.sjsu.cs146.homework.hw1;
+
+public class hw1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/sjsu/cs146/labs/lab1/lab1.java
@@ -0,0 +1,7 @@
+package com.sjsu.cs146.labs.lab1;
+
+public class lab1 {
+    public static void main(String[] args) {
+
+    }
+}
