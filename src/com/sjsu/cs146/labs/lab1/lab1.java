f9bd0d188585b792c8a4b716a44a98c79fedcbb5
Ryan Rajaie <ryan.rajaie04@gmail.com>
2024-01-31 17:15:06 -0800

    added stuff to most files

--- src/com/sjsu/cs146/labs/lab1/lab1.java
+++ src/com/sjsu/cs146/labs/lab1/lab1.java
@@ -1,7 +1,20 @@
 package com.sjsu.cs146.labs.lab1;
 
+import java.util.HashMap;
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+ */
 public class lab1 {
     public static void main(String[] args) {
+        int[] nums = new int[10];
+        System.out.println(twoSum(nums));
+    }
+    public static String twoSum(int[] nums){
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        return "null";
     }
 }
--- src/com/sjsu/cs146/labs/lab2/lab2.java
+++ src/com/sjsu/cs146/labs/lab2/lab2.java
@@ -2,6 +2,12 @@ package com.sjsu.cs146.labs.lab2;
 
 import java.util.HashMap;
 
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+typically using all the original letters exactly once.
+ */
+
 public class lab2 {
     public static void main(String[] args) {
         String s = "abcde";
@@ -32,7 +38,6 @@ public class lab2 {
                 tMap.put(t.charAt(j), 0);
             }
         }
-
         return sMap.equals(tMap);
     }
 }
