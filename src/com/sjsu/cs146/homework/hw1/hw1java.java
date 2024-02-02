d999f8480f5463494e74d5b7fb31322a7027bd45
Ryan Rajaie <ryan.rajaie04@gmail.com>
2024-02-01 16:55:43 -0800

    hw1

--- src/com/sjsu/cs146/homework/hw1/hw1java.java
+++ src/com/sjsu/cs146/homework/hw1/hw1java.java
@@ -10,11 +10,25 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 
 public class hw1java {
     public static void main(String[] args) {
-        String s = "";
+        String s = "Abba";
         System.out.println(isPalindrome(s));
     }
     public static boolean isPalindrome(String s){
-
-        return true;
+        s = s.toLowerCase();
+        System.out.println(s + "");
+        return isPalindromeRecurse(s);
+    }
+    public static boolean isPalindromeRecurse(String s){
+        if(s.length() <= 1){
+            return true;
+        }
+        else{
+            if(s.charAt(0) == s.charAt(s.length() - 1)){
+                return isPalindrome(s.substring(1, s.length() - 1));
+            }
+            else{
+                return false;
+            }
+        }
     }
 }
