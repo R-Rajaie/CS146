b399b6eec4c12b885132d6fa6ca7847f6f283522
Ryan Rajaie <ryan.rajaie04@gmail.com>
2024-02-04 18:04:52 -0800

    did hw1 py

--- src/com/sjsu/cs146/homework/hw1/hw1py.py
+++ src/com/sjsu/cs146/homework/hw1/hw1py.py
@@ -3,3 +3,24 @@
 # it reads the same forward and backward.
 # Alphanumeric characters include letters and numbers.
 # Given a string s, return true if it is a palindrome, or false otherwise.
+
+s = "friirfe"
+print(s + " is palindrome: ")
+
+str = ""
+
+for char in s:
+    if char.isalnum():
+        str += char.lower()
+
+def palinLoop(str):
+    if(len(str) <= 1):
+        print("true")
+        return
+    if(str[0] != str[len(str) - 1]):
+        print("false")
+        return
+    str = str[1:len(str) - 1]
+    palinLoop(str)
+
+palinLoop(str)
