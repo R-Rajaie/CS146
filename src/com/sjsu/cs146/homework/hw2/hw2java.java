939cce227b02a6b04f397337c343a6c81bfa61c9
Ryan Rajaie <ryan.rajaie04@gmail.com>
2024-02-07 12:01:06 -0800

    did h2j, started h3

--- src/com/sjsu/cs146/homework/hw2/hw2java.java
+++ src/com/sjsu/cs146/homework/hw2/hw2java.java
@@ -12,9 +12,29 @@ Implement a function to find the first bad version.
 You should minimize the number of calls to the API.
  */
 
+import java.util.Arrays;
+
 public class hw2java {
     public static void main(String[] args) {
+        boolean[] versions = new boolean[30];
+        int firstBad = 29;
 
+        Arrays.fill(versions, firstBad, versions.length, true);
+
+        int version = findBad(versions);
+        System.out.println("The first bad version is: " + (version + 1) + " [index: " + version + "]");
+    }
+    public static int findBad(boolean[] versions){
+        int first = 0;
+        int last = versions.length;
+        while(first < last){
+            int middle = first + (last - first) / 2;
+
+            if(isBad(versions[middle])) last = middle;
+            else first = middle + 1;
+        }
+        return first;
     }
 
+    public static boolean isBad(boolean b){ return b; }
 }
