a1fbc92b6deae9fe8b0d41ce7c1042cdf4240456
Ryan Rajaie <ryan.rajaie04@gmail.com>
2024-02-21 23:51:45 -0800

    hw6

--- src/com/sjsu/cs146/homework/hw6/hw6.java
+++ src/com/sjsu/cs146/homework/hw6/hw6.java
@@ -3,33 +3,36 @@ import java.util.*;
 
 public class hw6 {
     public static void main(String[] args) {
-        int[] list = new int[]{-1,0,1,2,-1,-4};
+        int[] list = new int[]{-1,0,1,2,-1,-4,2};
         System.out.println(threeSum(list));
     }
     public static List<List<Integer>> threeSum(int[] nums){
         HashSet<Integer> set = new HashSet<>();
         HashSet<List<Integer>> nonDupList = new HashSet<>();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int n : nums) list.add(n);
 
         for(int num : nums) set.add(num);
 
         for (int num : set) {
-            int[] pair = twoSum(nums, -num);
+            list.remove(Integer.valueOf(num));
+            int[] pair = twoSum(list, -num);
             if (pair != null) {
-                ArrayList<Integer> trip = new ArrayList<>();
-                trip.addAll(List.of(num, pair[0], pair[1]));
+                ArrayList<Integer> trip = new ArrayList<>(List.of(num, pair[0], pair[1]));
                 Collections.sort(trip);
                 nonDupList.add(trip);
             }
         }
         return new ArrayList<>(nonDupList);
     }
-    public static int[] twoSum(int[] nums, int target){
+    public static int[] twoSum(ArrayList<Integer> nums, int target){
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], i);
-            if(map.containsKey(target - nums[i]) && (target - nums[i]) != nums[i]){
-                return new int[]{nums[i],target - nums[i]};
+        for(int i = 0; i < nums.size(); i++){
+            map.put(nums.get(i), i);
+            if(map.containsKey(target - nums.get(i)) && (target - nums.get(i)) != nums.get(i)){
+                return new int[]{nums.get(i),target - nums.get(i)};
             }
         }
         return null;
