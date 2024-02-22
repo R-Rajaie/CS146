6d1dc41a50d2ed8a3ddd2f23308e677d93577325
Ryan Rajaie <ryan.rajaie04@gmail.com>
2024-02-22 04:47:04 -0800

    hw6

--- src/com/sjsu/cs146/homework/hw6/hw6.py
+++ src/com/sjsu/cs146/homework/hw6/hw6.py
@@ -2,27 +2,26 @@ from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         manifest = set()
-        array = []
         nonDup = []
     
         for num in nums:
             manifest.add(num)
-            array.append(num)
         
         for num in manifest:
-            array.remove(num)
+            nums.remove(num)
             map = {}
             for i in range (0, len(nums) - 1):
                 if not nums[i] in map.keys():
                     map.update({nums[i] : i})
                     target = -(num + nums[i])
                 
-                if target in map.keys() and map.get(target) != i:
-                    trip = [num, array[i], target]
+                if target in map.keys():
+                    trip = [num, nums[i], target]
                     trip.sort()
                     nonDup.append(trip)
-            array.append(num)
-        return nonDup
+            nums.append(num)
+        tuples = {tuple(inner) for inner in nonDup}
+        return [list(inner) for inner in tuples]
 
 soln = Solution()
-print(soln.threeSum([-1,0,1,2,-1,-4]))
+print(soln.threeSum([-1,0,1,2,-1,-4,2]))
