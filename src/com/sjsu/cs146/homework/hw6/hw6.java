f9791e0d7eb97d51ecd7a244c36a4f1df0ce0a0a
Ryan Rajaie <ryan.rajaie04@gmail.com>
2024-02-22 01:39:32 -0800

    hw6

--- src/com/sjsu/cs146/homework/hw6/hw6.java
+++ src/com/sjsu/cs146/homework/hw6/hw6.java
@@ -1,42 +1,51 @@
+// this is the most incomprehensible spaghetti piece of shit ive ever written in my life bro
+
 package com.sjsu.cs146.homework.hw6;
 import java.util.*;
 
 public class hw6 {
     public static void main(String[] args) {
-        int[] list = new int[]{-1,0,1,2,-1,-4, 0, 0};
+        int[] list = new int[]{-1,0,1,2,-1,-4};
         System.out.println(threeSum(list));
     }
     public static List<List<Integer>> threeSum(int[] nums){
+        // make a hashset
         HashSet<Integer> set = new HashSet<>();
-        HashSet<List<Integer>> nonDupList = new HashSet<>();
+
+        // ok now a list
         ArrayList<Integer> list = new ArrayList<>();
 
+        // fuck it make another hashset
+        HashSet<List<Integer>> nonDupList = new HashSet<>();
+
+        // we love loops. the time complexity is real cool on this one
         for(int num : nums){
             list.add(num);
             set.add(num);
         }
 
+        // this for loop being shorter than O(n) meant something once upon a time when i cared about the efficiency
         for (int num : set) {
             list.remove(Integer.valueOf(num));
-            int[] pair = twoSum(list, -num);
-            if (pair != null) {
-                ArrayList<Integer> trip = new ArrayList<>(List.of(num, pair[0], pair[1]));
-                Collections.sort(trip);
-                nonDupList.add(trip);
-                System.out.println(num + " " + trip);
+
+            // fuck it we need a hashmap now.
+            HashMap<Integer, Integer> map = new HashMap<>();
+
+            // this is twosum. take an advil before reading.
+            for(int i = 0; i < list.size(); i++){
+
+                // we can track the first index the number shows up. this becomes important later on.
+                if(!map.containsKey(list.get(i))) map.put(list.get(i), i);
+
+                // this used to be a twosum method that i butchered up. as you can see it went great.
+                if(map.containsKey(-(num + list.get(i))) && map.get(-(num + list.get(i))) != i){
+                    ArrayList<Integer> trip = new ArrayList<>(List.of(num, list.get(i), -(num + list.get(i))));
+                    Collections.sort(trip);
+                    nonDupList.add(trip);
+                }
             }
+
         }
         return new ArrayList<>(nonDupList);
-    }
-    public static int[] twoSum(ArrayList<Integer> nums, int target){
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for(int i = 0; i < nums.size(); i++){
-            map.put(nums.get(i), i);
-            if(map.containsKey(target - nums.get(i))){
-                return new int[]{nums.get(i),target - nums.get(i)};
-            }
-        }
-        return null;
     }
 }
