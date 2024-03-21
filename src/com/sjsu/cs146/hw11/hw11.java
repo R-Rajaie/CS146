d2e12c3764337ca5db5aa861c88875e08df27d8d
Ryan Rajaie <ryan.rajaie04@gmail.com>
2024-03-20 23:32:23 -0700

    hw 11

--- src/com/sjsu/cs146/hw11/hw11.java
+++ src/com/sjsu/cs146/hw11/hw11.java
@@ -1,12 +1,57 @@
 package com.sjsu.cs146.hw11;
 
+import java.util.*;
+import java.awt.Point;
+
 public class hw11 {
     public static void main(String[] args) {
-
+        int[][] img ={
+                {1,1,1},
+                {1,1,0},
+                {1,0,1}
+        };
+        floodFill(img, 1, 1, 2);
+        for (int[] array:img) {
+            for (int i:array) System.out.print(i);
+            System.out.println();
+        }
     }
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        // this is a graph problem...
-        // make queue, continuously fill it with correct color neighbors, change
+    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
+       return helper(image, image.length, image[0].length, sr, sc, image[sr][sc], color);
+    }
+    public static int[][] helper(int[][] image, int m, int n, int sr, int sc, int num, int color){
+        Vector<Point> queue = new Vector<>();
+        queue.add(new Point(sr, sc));
+        image[sr][sc] = color;
+
+        while(!(queue.isEmpty())){
+            Point current = queue.get(queue.size() - 1);
+            queue.remove(queue.size() - 1);
+
+            int x = current.x;
+            int y = current.y;
+
+            if(isIn(image, m, n, x + 1, y, num, color)){
+                image[x+1][y] = color;
+                queue.add(new Point(x + 1, y));
+            }
+            if(isIn(image, m, n, x - 1, y, num, color)){
+                image[x-1][y] = color;
+                queue.add(new Point(x - 1, y));
+            }
+            if(isIn(image, m, n, x, y + 1, num, color)){
+                image[x][y+1] = color;
+                queue.add(new Point(x, y + 1));
+            }
+            if(isIn(image, m, n, x, y - 1, num, color)){
+                image[x][y-1] = color;
+                queue.add(new Point(x, y - 1));
+            }
+        }
+
         return image;
+    }
+    public static boolean isIn(int[][] image, int m, int n, int sr, int sc, int num, int color){
+        return !(sr < 0 || sr >= m || sc < 0 || sc >= n || image[sr][sc] != num || image[sr][sc] == color);
     }
 }
---
+++ src/com/sjsu/cs146/labs/lab6/lab6.java
@@ -0,0 +1,11 @@
+package com.sjsu.cs146.labs.lab6;
+
+public class lab6 {
+    public static void main(String[] args) {
+
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // essentially asking if this is a cyclic graph
+        return true;
+    }
+}
