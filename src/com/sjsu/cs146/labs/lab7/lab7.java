d4950f13df2fd2a27e8601bc024c4e7ba32c8bb5
Ryan Rajaie <ryan.rajaie04@gmail.com>
2024-04-23 02:15:00 -0700

    lab7

--- src/com/sjsu/cs146/labs/lab7/lab7.java
+++ src/com/sjsu/cs146/labs/lab7/lab7.java
@@ -6,31 +6,48 @@ public class lab7 {
     public static void main(String[] args) {
 
     }
+
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        int [][] g = new int[n][n];
-        int[] dist = new int[n];
-        boolean[] vis = new boolean[n];
+        int[][] graph = new int[n][n];
+        int[] distances = new int[n];
+        boolean[] visited = new boolean[n];
 
-        for(int[] e : g) Arrays.fill(e, 1 << 30);
+        for (var e : graph) Arrays.fill(e, 1 << 30);
 
-        for(int[] e : edges){
-            int f = e[0];
-            int t = e[1];
-            int w = e[2];
+        for (var e : edges) {
+            int a = e[0];
+            int b = e[1];
+            int c = e[2];
+            graph[a][b] = c;
+            graph[b][a] = c;
         }
         int ans = n;
         int cnt = 1 << 30;
+        for (int i = n - 1; i >= 0; i--) {
+            int t = dijkstra(i, distances, n, visited, distanceThreshold, graph);
+            if (t < cnt) {
+                cnt = t;
+                ans = i;
+            }
+        }
+        return ans;
+    }
 
-        for(int i = n - 1; i >= 0; i--){
+    private int dijkstra(int u, int[] distances, int n, boolean[] visited, int distanceThreshold, int[][] graph) {
+        Arrays.fill(distances, 1 << 30);
+        Arrays.fill(visited, false);
+        distances[u] = 0;
 
+        for (int i = 0; i < n; ++i) {
+            int k = -1;
+            for (int j = 0; j < n; ++j)  if (!visited[j] && (k == -1 || distances[k] > distances[j])) k = j;
+            visited[k] = true;
+            for (int j = 0; j < n; ++j) distances[j] = Math.min(distances[j], distances[k] + graph[k][j]);
         }
 
-        return 0;
-    }
-
-    public int dijkstra(int u){
-       int cnt = 0;
-
-       return cnt;
+        int cnt = 0;
+        for (int d : distances) if (d <= distanceThreshold) ++cnt;
+        return cnt;
     }
 }
+
